67cc67cf397279b063ffc2e0dd5cd55663c4e051
Nima Mahmoudi <nima_mahmoudi@live.com>
2023-11-08 19:59:11 -0500

    added file embedding

--- app.py
+++ app.py
@@ -1,71 +1,89 @@
 import streamlit as st
 import os
+import embed_pdf
 
 # create sidebar and ask for openai api key if not set in secrets
-if "OPENAI_API_KEY" in st.secrets:
-  os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+secrets_file_path = os.path.join(".streamlit", "secrets.toml")
+if os.path.exists(secrets_file_path):
+    try:
+        if "OPENAI_API_KEY" in st.secrets:
+            os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+        else:
+            print("OpenAI API Key found in environment variables")
+    except FileNotFoundError:
+        print('Secrets file not found')
 else:
-  os.environ["OPENAI_API_KEY"] = st.sidebar.text_input("OpenAI API Key",
-                                                       type="password")
+    print('Secrets file not found')
+
+if not os.getenv('OPENAI_API_KEY', '').startswith("sk-"):
+    os.environ["OPENAI_API_KEY"] = st.sidebar.text_input(
+        "OpenAI API Key", type="password"
+    )
+else:
+    if st.sidebar.button("Embed Documents"):
+        st.sidebar.info("Embedding documents...")
+        try:
+            embed_pdf.embed_all_pdf_docs()
+            st.sidebar.info("Done!")
+        except Exception as e:
+            st.sidebar.error(e)
+            st.sidebar.error("Failed to embed documents.")
+
+chosen_file = st.radio(
+    "Choose a file to search", embed_pdf.get_all_index_files(), index=0
+)
 
 # create the app
 st.title("Welcome to NimaGPT")
 
 # check if openai api key is set
-if "OPENAI_API_KEY" not in os.environ or not os.environ[
-    "OPENAI_API_KEY"].startswith("sk-"):
-  st.warning("Please enter your OpenAI API key!", icon="⚠")
-  st.stop()
+if not os.getenv('OPENAI_API_KEY', '').startswith("sk-"):
+    st.warning("Please enter your OpenAI API key!", icon="⚠")
+    st.stop()
 
 # load the agent
 from llm_helper import convert_message, get_rag_chain
 
-custom_chain = get_rag_chain()
+custom_chain = get_rag_chain(chosen_file)
 
 # create the message history state
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+    st.session_state.messages = []
 
 # render older messages
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # render the chat input
 prompt = st.chat_input("Enter your message...")
 if prompt:
-  st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-  # render the user's new message
-  with st.chat_message("user"):
-    st.markdown(prompt)
+    # render the user's new message
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-  # render the assistant's response
-  with st.chat_message("assistant"):
-    message_placeholder = st.empty()
+    # render the assistant's response
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
 
-    if "messages" in st.session_state:
-      chat_history = [
-          convert_message(m) for m in st.session_state.messages[:-1]
-      ]
-    else:
-      chat_history = []
+        if "messages" in st.session_state:
+            chat_history = [convert_message(m) for m in st.session_state.messages[:-1]]
+        else:
+            chat_history = []
 
-    full_response = ""
-    for response in custom_chain.stream({
-        "input": prompt,
-        "chat_history": chat_history
-    }):
-      if "output" in response:
-        full_response += response["output"]
-      else:
-        full_response += response.content
+        full_response = ""
+        for response in custom_chain.stream(
+            {"input": prompt, "chat_history": chat_history}
+        ):
+            if "output" in response:
+                full_response += response["output"]
+            else:
+                full_response += response.content
 
-      message_placeholder.markdown(full_response + "▌")
-    message_placeholder.markdown(full_response)
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
 
-  # add the full response to the message history
-  st.session_state.messages.append({
-      "role": "assistant",
-      "content": full_response
-  })
+    # add the full response to the message history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
--- llm_helper.py
+++ llm_helper.py
@@ -10,41 +10,40 @@ from langchain.schema.messages import HumanMessage, SystemMessage, AIMessage
 
 
 def format_docs(docs):
-  res = ""
-  # res = str(docs)
-  for doc in docs:
-    escaped_page_content = doc.page_content.replace('\n', '\\n')
-    res += "<doc>\n"
-    res += f"  <content>{escaped_page_content}</content>\n"
-    for m in doc.metadata:
-      res += f"  <{m}>{doc.metadata[m]}</{m}>\n"
-    res += "</doc>\n"
+    res = ""
+    # res = str(docs)
+    for doc in docs:
+        escaped_page_content = doc.page_content.replace("\n", "\\n")
+        res += "<doc>\n"
+        res += f"  <content>{escaped_page_content}</content>\n"
+        for m in doc.metadata:
+            res += f"  <{m}>{doc.metadata[m]}</{m}>\n"
+        res += "</doc>\n"
+    return res
 
-  return res
 
+def get_search_index(file_name="Mahmoudi_Nima_202202_PhD.pdf", index_folder="index"):
+    # load embeddings
+    from langchain.vectorstores import FAISS
+    from langchain.embeddings.openai import OpenAIEmbeddings
 
-def get_search_index():
-  # load embeddings
-  from langchain.vectorstores import FAISS
-  from langchain.embeddings.openai import OpenAIEmbeddings
-
-  file_folder = 'index'
-  file_name = 'Mahmoudi_Nima_202202_PhD.pdf'
-  search_index = FAISS.load_local(folder_path=file_folder,
-                                  index_name=file_name + '.index',
-                                  embeddings=OpenAIEmbeddings())
-  return search_index
+    search_index = FAISS.load_local(
+        folder_path=index_folder,
+        index_name=file_name + ".index",
+        embeddings=OpenAIEmbeddings(),
+    )
+    return search_index
 
 
 def convert_message(m):
-  if m['role'] == 'user':
-    return HumanMessage(content=m['content'])
-  elif m['role'] == 'assistant':
-    return AIMessage(content=m['content'])
-  elif m['role'] == 'system':
-    return SystemMessage(content=m['content'])
-  else:
-    raise ValueError(f"Unknown role {m['role']}")
+    if m["role"] == "user":
+        return HumanMessage(content=m["content"])
+    elif m["role"] == "assistant":
+        return AIMessage(content=m["content"])
+    elif m["role"] == "system":
+        return SystemMessage(content=m["content"])
+    else:
+        raise ValueError(f"Unknown role {m['role']}")
 
 
 _condense_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
@@ -64,33 +63,34 @@ ANSWER_PROMPT = ChatPromptTemplate.from_template(_rag_template)
 
 
 def _format_chat_history(chat_history):
+    def format_single_chat_message(m):
+        if type(m) is HumanMessage:
+            return "Human: " + m.content
+        elif type(m) is AIMessage:
+            return "Assistant: " + m.content
+        elif type(m) is SystemMessage:
+            return "System: " + m.content
+        else:
+            raise ValueError(f"Unknown role {m['role']}")
 
-  def format_single_chat_message(m):
-    print(m)
-    if type(m) is HumanMessage:
-      return "Human: " + m.content
-    elif type(m) is AIMessage:
-      return "Assistant: " + m.content
-    elif type(m) is SystemMessage:
-      return "System: " + m.content
-    else:
-      raise ValueError(f"Unknown role {m['role']}")
-
-  return "\n".join([format_single_chat_message(m) for m in chat_history])
+    return "\n".join([format_single_chat_message(m) for m in chat_history])
 
 
-def get_rag_chain():
-  vectorstore = get_search_index()
-  retriever = vectorstore.as_retriever()
+def get_rag_chain(file_name="Mahmoudi_Nima_202202_PhD.pdf", index_folder="index"):
+    vectorstore = get_search_index(file_name, index_folder)
+    retriever = vectorstore.as_retriever()
 
-  _inputs = RunnableMap(standalone_question=RunnablePassthrough.assign(
-      chat_history=lambda x: _format_chat_history(x["chat_history"]))
-                        | CONDENSE_QUESTION_PROMPT
-                        | ChatOpenAI(temperature=0)
-                        | StrOutputParser(), )
-  _context = {
-      "context": itemgetter("standalone_question") | retriever | format_docs,
-      "question": lambda x: x["standalone_question"],
-  }
-  conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
-  return conversational_qa_chain
+    _inputs = RunnableMap(
+        standalone_question=RunnablePassthrough.assign(
+            chat_history=lambda x: _format_chat_history(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | ChatOpenAI(temperature=0)
+        | StrOutputParser(),
+    )
+    _context = {
+        "context": itemgetter("standalone_question") | retriever | format_docs,
+        "question": lambda x: x["standalone_question"],
+    }
+    conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
+    return conversational_qa_chain
